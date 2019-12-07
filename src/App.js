619bbbbd2f4b87c05be827ebe0200520f018e56b
AISHA-PRO <aisha1rashed@gmail.com>
2019-12-07 14:49:18 +0300

    fixErrorApp

--- src/App.js
+++ src/App.js
@@ -3,45 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      newTask: '',
-      newDate: '',
-      tasks: []
-    }
-  };
-
-  handleFormSubmit = (e) => {
-    e.preventDefault();
-
-    let tasks = [...this.state.tasks];
-
-    tasks.push({newTask: this.state.newTask, newDate: this.state.newDate});
-
-    this.setState({
-      tasks,
-      newTask: '',
-      newDate: ''
-    });
-  };
-
-  handleInputChange = (e) => {
-    let input = e.target;
-    let name = e.target.name;
-    let value = input.value;
-
-    this.setState({
-      [name]: value
-    })
-  };
-
+ 
   render() {
     return (
       <div className="App">
-        <Container handleFormSubmit={ this.handleFormSubmit } handleInputChange={ this.handleInputChange } 
-        newTask={ this.state.newTask } newDate={ this.state.newDate } tasks={ this.state.tasks }/>
+        <Container/>
         
       </div>
     );
--- src/Container.jsx
+++ src/Container.jsx
@@ -4,12 +4,45 @@ import ToDoList from './ToDoList'
 
 export class Container extends Component {
  
-
+    constructor() {
+        super();
+    
+        this.state = {
+          newTask: '',
+          newDate: '',
+          tasks: []
+        }
+      };
+    
+      handleFormSubmit = (e) => {
+        e.preventDefault();
+    
+        let tasks = [...this.state.tasks];
+    
+        tasks.push({newTask: this.state.newTask, newDate: this.state.newDate});
+    
+        this.setState({
+          tasks,
+          newTask: '',
+          newDate: ''
+        });
+      };
+    
+      handleInputChange = (e) => {
+        let input = e.target;
+        let name = e.target.name;
+        let value = input.value;
+    
+        this.setState({
+          [name]: value
+        })
+      };
+    
     render() {
         return (
             <div>
                 <Form2 handleFormSubmit={ this.handleFormSubmit } handleInputChange={ this.handleInputChange } 
-        newTask={ this.state.newTask } newDate={ this.state.newDate } />
+                newTask={ this.state.newTask } newDate={ this.state.newDate } />
                 <ToDoList tasks={ this.state.tasks }/>
             </div>
         )
