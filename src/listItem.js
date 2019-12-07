74102a49222eb9f463f3a7b1f6eef2a1a2a6284b
EmanAlzariey <eman_alzariey@yahoo.com>
2019-12-07 15:05:30 +0300

    div

--- src/listItem.js
+++ src/listItem.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react'
 export class listItem extends Component {
     render() {
         return (
+            <div>
             <li>
                {this.props.newTask} 
             </li>
             <button onClick={()=>{this.props.editTask(this.props.id)}} />
+            </div>
         )
     }
 }
